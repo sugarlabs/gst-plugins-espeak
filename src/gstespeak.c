ffee3f5dacaf0ee090986299a5f35c0931b64946
Aleksey Lim <alsroot@member.fsf.org>
2009-03-14 05:42:38 +0000

    Build against gstreamer-0.10.12

--- src/gstespeak.c
+++ src/gstespeak.c
@@ -79,18 +79,21 @@ GST_BOILERPLATE_FULL(GstEspeak, gst_espeak, GstBaseSrc, GST_TYPE_BASE_SRC,
 /******************************************************************************/
 
 static void
-gst_espeak_base_init (gpointer gclass)
+gst_espeak_base_init(gpointer gclass)
 {
-  GstElementClass *element_class = GST_ELEMENT_CLASS (gclass);
+    GstElementClass *element_class = GST_ELEMENT_CLASS(gclass);
 
-  gst_element_class_set_details_simple(element_class,
-    "Espeak",
-    "FIXME:Generic",
-    "FIXME:Generic Template Element",
-    " <<user@hostname.org>>");
+    static GstElementDetails details = {
+        "Espeak",
+        "Source",
+        "Uses eSpeak library as a sound source for GStreamer",
+        "Aleksey S. Lim <alsroot@member.fsf.org>"
+    };
 
-  gst_element_class_add_pad_template (element_class,
-      gst_static_pad_template_get (&src_factory));
+    gst_element_class_set_details(element_class, &details);
+
+    gst_element_class_add_pad_template(element_class,
+            gst_static_pad_template_get(&src_factory));
 }
 
 /* initialize the espeak's class */
