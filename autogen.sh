91f3bb219a6d8dfeef8539e1143f59f4f5099796
Daniel Narvaez <dwnarvaez@gmail.com>
2013-06-21 22:59:55 +0200

    Modernize the autogen.sh script
    It was failing to find the m4 macros directory with automake
    1.13.2. This is exactly the same autogen.sh we are using in
    all the core sugar modules.

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-aclocal -I m4/ $ACLOCAL_FLAGS --install || exit 1
-libtoolize --copy --force || exit 1
-autoheader || exit 1
-autoconf || exit 1
-automake -a -c || exit 1
-
-[ "$NOCONFIGURE" ] || ./configure CFLAGS="$CFLAGS -Wall" --enable-maintainer-mode "$@"
+srcdir=`dirname "$0"`
+cd "$srcdir"
+autoreconf -i -f
+cd -
+[ "$NOCONFIGURE" ] || "$srcdir"/configure CFLAGS="$CFLAGS -Wall" --enable-maintainer-mode "$@"
