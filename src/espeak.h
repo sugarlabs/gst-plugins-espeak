b8e815a1b8ca1468d92ab8631779e3ee3d832613
Aleksey Lim <alsroot@sugarlabs.org>
2018-06-08 08:43:05 +1000

    Add copyright

--- src/espeak.h
+++ src/espeak.h
@@ -1,4 +1,6 @@
 /*
+ * Copyright (C) 2009, Aleksey Lim <alsroot@sugarlabs.org>
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
  * License as published by the Free Software Foundation; either
